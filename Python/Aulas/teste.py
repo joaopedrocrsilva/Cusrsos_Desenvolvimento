cf1e7ab903f3c245ef0503c914c4304ad8a887cf
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-04 17:15:52 -0300

    main

---
+++ Python/Aulas/teste.py
@@ -0,0 +1,20 @@
+n = 1 #contador
+soma = 0 #acumulador 
+while n <= 10:
+    x = int(input(f'{n} número: '))
+    soma = soma + x
+    n = n + 1
+print(f'Soma: {soma}')
+
+n = 1 #contador
+fat = int(input('numero:'))
+while n <= fat:
+    fat = fat * n
+    n = n + 1
+print (f're(fat) = {re}')
+
+
+
+
+
+
