9eec4718742ec515d034d7469992acf1bf337250
joaopedrocrsilva <joaopedrocrsilva@hotmail.com>
2021-05-06 18:27:27 -0300

    main

---
+++ Python/Lista3/exercicio1.py
@@ -0,0 +1,7 @@
+nota = float(input('Nota: '))
+
+while nota < 0  or nota > 10:
+    print('Nota invalida')
+    nota = float(input('Digite uma nota'))
+   
+
---
+++ Python/Lista3/exercicio2.py
@@ -0,0 +1,8 @@
+nome = input('Nome')
+senha = input('Senha')
+
+while (nome == senha):
+    print('Senha nao pode ser a mesma do nome')
+    nome = input('Nome')
+    senha = input('Senha')
+    
